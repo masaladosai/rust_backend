0b6c13659288ecefa0c247668b07d1919d5f2c1e
masaladosai <jhageee@gmail.com>
2025-09-23 13:47:49 +0530

    added json

--- src/mod.rs
+++
@@ -1,4 +0,0 @@
-pub fn route()->Router{
-     Router::new().route("/",get(get_index).post(post_index))
-.route("/fun",get(test))
-}
--- src/routes/login.rs
+++ src/routes/login.rs
@@ -1,9 +1,33 @@
+use axum::Json;
+use serde::{Deserialize,Serialize};
+
+
+
+
+
+#[derive(Deserialize,Serialize,Debug)]
+pub struct LogData{
+    username:String,
+    password:String,
+}
+
+#[derive(Serialize)]
+pub struct RespData{
+    message:String,
+    username:String
+}
+
 pub async fn login(){
     println!("login page");
 }
 
-pub async fn sign_up(){
-    println!("signup page");
+pub async fn sign_up(body:Json<LogData>)->Json<RespData>{
+
+    println!("signup page hit with json");
+    return Json(RespData{
+        message:"i like u".to_string(),
+        username:"yash".to_string()
+    })
 }
 
 
